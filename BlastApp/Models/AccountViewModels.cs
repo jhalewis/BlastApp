bb4528858b42a588048c2756bc9a9589aef3eb0c
James Lewis <jamesl@silverbridge.co.za>
2017-08-25 15:03:49 +0200

    removed 1

--- BlastApp/Models/AccountViewModels.cs
+++ BlastApp/Models/AccountViewModels.cs
@@ -71,7 +71,7 @@ namespace BlastApp.Models
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Emails1")]
+        [Display(Name = "Emails")]
         public string Email { get; set; }
 
         [Required]
